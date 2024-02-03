bd60feeadd3234724c92b0e6ba8767c64ec3ba71
Ahamed Arshad <achchu.zats@gmail.com>
2024-02-03 19:51:21 +0530

    style tweak in list and filter

--- views/frontend/grant/list.php
+++ views/frontend/grant/list.php
@@ -4,13 +4,22 @@
  */
 ?>
 <div class="grant-list-wrapper">
-	<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-		<div class="grant-list-item">
-			<hr>
-			<h2><?php the_title(); ?></h2>
-			<p><strong>Location:</strong> <?php echo esc_html( get_post_meta( get_the_ID(), 'grant_location', true ) ); ?></p>
-			<p><strong>Recipient:</strong> <?php echo esc_html( get_post_meta( get_the_ID(), 'grant_recipient', true ) ); ?></p>
-			<button><a href="<?php the_permalink(); ?>">Learn More</a></button>
-		</div>
-	<?php endwhile; ?>
+	<div class="grant-list-content">
+		<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+			<div class="grant-list-item">
+				<div class="left-part">
+					<h2><?php the_title(); ?></h2>
+					<p><strong>Location:</strong> <?php echo esc_html( get_post_meta( get_the_ID(), 'grant_location', true ) ); ?></p>
+					<p><strong>Recipient:</strong> <?php echo esc_html( get_post_meta( get_the_ID(), 'grant_recipient', true ) ); ?></p>
+				</div>
+				<div class="right-part">
+					<button><a href="<?php the_permalink(); ?>">Learn More</a></button>
+				</div>
+			</div>
+		<?php endwhile; ?>
+	</div>
+	<div class="grant-list-footer">
+		<button id="grant-list-full-view">View Full List</button>
+		<button id="grant-list-show-more">Show More</button>
+	</div>
 </div>
