8e726c1b3e5a6a14497f667c3232d0514d66a320
Ahamed Arshad <achchu.zats@gmail.com>
2024-02-04 00:13:49 +0530

    JS refactor

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -2,64 +2,91 @@
 
 	var filters = {};
 
+	/**
+	 * Add filter element as a chip.
+	 *
+	 * @param {string} key
+	 * @param {string} label
+	 */
 	function addFilterToList( key, label ) {
 		$( '.grant-filter-list' ).prepend( '<div class="grant-filter-list--item" data-key="'+ key +'"><button></button>'+ label +'</div>' );
 	}
 
+	/**
+	 * Remove filter element from the chip list.
+	 *
+	 * @param {string} key
+	 */
 	function removeFilterFromList( key ) {
 		$( '.grant-filter-list--item[data-key="'+key+'"]' ).remove();
 	}
 
-	function applyFilters() {
-		Object.entries( filters ).forEach( function( [ key, value ] ) {
-			var label = $( '.grant-custom-dropdown--list-item[data-value="' + value + '"]' ).text();
-			removeFilterFromList( key );
-			addFilterToList( key, label );
-		} );
+	/**
+	 * Remove selected filter value from the dropdown.
+	 *
+	 * @param {string} key
+	 */
+	function removeSelectedFilterValue( key ) {
+		$( '.grant-custom-dropdown--trigger[data-filter="'+key+'"]' ).next().find( '.is-active' ).removeClass( 'is-active' );
 	}
 
+	/**
+	 * Clear all filters.
+	 */
+	function clearFilters() {
+		$( '.grant-filter-list--item' ).remove();
+		$( '.grant-custom-dropdown--list-item.is-active' ).removeClass( 'is-active' );
+	}
+
+	/**
+	 * On document ready.
+	 */
 	$( function() {
 
+		// Filter dropdown interaction.
 		$( '.grant-custom-dropdown' ).on( 'click', '.grant-custom-dropdown--trigger', function( e ) {
 			e.preventDefault();
 
 			var dropdown = $( this ).closest( '.grant-custom-dropdown' );
-
 			$( '.grant-custom-dropdown' ).not( dropdown ).removeClass( 'is-open' );
 			dropdown.toggleClass( 'is-open' );
 		} );
 
+		// Filter dropdown list item interaction.
 		$( '.grant-custom-dropdown--list-item' ).on( 'click', function( e ) {
 
 			var filter = $( this ).parent().prev().data( 'filter' );
 			var value = $( this ).data( 'value' );
+			var label = $( this ).text();
 
 			if ( filter && value ) {
 
-				$( this )
-					.addClass( 'is-active' )
-					.siblings().removeClass( 'is-active' );
-
 				filters[filter] = value;
 
-				applyFilters();
+				$( this ).addClass( 'is-active' ).siblings().removeClass( 'is-active' );
+
+				removeFilterFromList( filter );
+				addFilterToList( filter, label );
 			}
 		} );
 
+		// Handle filter chip remove.
 		$( '.grant-filter-list' ).on( 'click', '.grant-filter-list--item button', function( e ) {
 			e.preventDefault();
 
 			var key = $( this ).parent().data( 'key' );
-			removeFilterFromList( key );
 			delete filters[key];
-			applyFilters();
+
+			removeFilterFromList( key );
+			removeSelectedFilterValue( key );
 		} );
 
 
+		// Handle clear filter button.
 		$( '#clear-filter' ).on( 'click', function() {
-			$( '.grant-filter-list--item' ).remove();
 			filters = {};
-		} )
+			clearFilters();
+		} );
 
 	} );
 
